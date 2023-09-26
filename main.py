ec26c277906a704703674b06b03578aaffc92187
Eric Tam <cowbaz@gmail.com>
2023-09-26 22:06:14 +0100

    Revised main.py

--- main.py
+++ main.py
@@ -1,164 +1,110 @@
-from transitions import Machine
 from motion_detection import picam2
 from object_detecting import image_pred
 from object_detected import save_to_csv
 from edgetpumodel import EdgeTPUModel
 import time
 
+# Define variables for use use in handlers
+Image_Buffer = None  # declare image buffer for captured image
+Pred = None  # the prediction in each frame
+File_Path = "./data/images"  # the file path of the captured image if saved 
+Idle_Max = 5  # where 5 frame no motion detected, then object detection again
+TIME_DELAY = 1 # setting time sleep (in sec) in idle stage
 
-class PiObjectDetection:
+# A simple Finite State Machine Class
+class fsm:
     """
-    A class for object detection.
+    A simple state machine implementation.
+
+    This class allows you to manage a finite state machine with various states 
+    and associated handler functions.
 
     Attributes:
-        states (List[str]): The various states in object detection
-
+        handlers (dict): A dictionary mapping state names to handler functions.
     """
-
     def __init__(self):
-        self.states = [
-            "motion_detect",
-            "motion_not_detected",
-            "object_detecting",
-            "object_detected",
-            "idle",
-        ]
-        self.machine = Machine(model=self, states=self.states, initial="idle")
+        self.handlers = {}
 
-        # Initialize context variable
-        self.context = {}
-        self.context["count"] = 0
+    def add_state(self, name, handler):
+        self.handlers[name] = handler
 
-        # Initialize the transition flow from stage to stage
-        self.machine.add_transition(
-            "ch_to_motion_n_detected",
-            "motion_detect",
-            "motion_not_detected",
-            before="store_data",
-        )
-        self.machine.add_transition(
-            "ch_to_object_detecting",
-            ["motion_detect", "motion_not_detected"],
-            "object_detecting",
-            before="store_data",
-        )
-        self.machine.add_transition(
-            "ch_to_object_detected",
-            "object_detecting",
-            "object_detected",
-            before="store_data",
-        )
-        self.machine.add_transition(
-            "ch_to_idle",
-            ["object_detected", "motion_not_detected"],
-            "idle",
-            before="store_data",
-        )
-        self.machine.add_transition(
-            "ch_to_motion_detect", "idle", "motion_detect", before="store_data"
-        )
-        self.machine.add_transition(
-            "ch_to_motion_detect",
-            "object_detecting",
-            "motion_detect",
-            before="store_data",
-        )
-
-    def store_data(self):
-        self.context["previous_state"] = self.state
-
-    # Actions on entering into different states
-    def on_enter_idle(self):
-        print(
-            "Previous state is {}, now is {} now.".format(
-                self.context["previous_state"], self.state
-            )
-        )
-
-    def on_enter_motion_detect(self):
-        print(
-            "Previous state is {}, now is {} state now.".format(
-                self.context["previous_state"], self.state
-            )
-        )
-
-    def on_enter_motion_n_detected(self):
-        print(
-            "Previous state is {}, now is {} state now.".format(
-                self.context["previous_state"], self.state
-            )
-        )
-
-    def on_enter_object_detecting(self):
-        print(
-            "Previous state is {}, now is {} state now.".format(
-                self.context["previous_state"], self.state
-            )
-        )
-
-    def on_enter_object_detected(self):
-        print(
-            "Previous state is {}, now is {} state now.".format(
-                self.context["previous_state"], self.state
-            )
-        )
-
-    # Loop for object detection
-    def run(self):
+    def run(self, startingState, idle_count=0, motion_flag=False, object_flag=False):
+        handler = self.handlers[startingState]
         while True:
-            PiRun.ch_to_motion_detect()  # start the intial stage of the loop
-            if picam.motion_detection():  # check motion is detected or not
-                np_image = (
-                    picam.capture_image()
-                )  # return a fresh capture in ndarray image
-                PiRun.context["count"] = 0
-                PiRun.ch_to_object_detecting()
-                pred, file_path = image_pred(
-                    yolov5s_224_edge, np_image
-                )  # output of detected bbox and file name
-                if len(pred) == 0:  # nothing detected
-                    pass
-                    # time.sleep(10)  # cooling time if needed
-                else:  # object detected
-                    PiRun.ch_to_object_detected()
-                    save_to_csv(
-                        file_path
-                    )  # save the summary of detection results in csv
-                    PiRun.ch_to_idle()
-                    time.sleep(1)  # cooling time if needed
-            else:
-                PiRun.ch_to_motion_n_detected()
-                PiRun.context["count"] += 1
-                if (
-                    PiRun.context["count"] <= 5
-                ):  # having no motion detected for 5 rounds, enter object detecting state
-                    PiRun.ch_to_idle()
-                    print("Idle Count: {} of 5".format(PiRun.context["count"]))
-                    time.sleep(1)
+            (newState, idle_count, motion_flag, object_flag) = handler(idle_count, 
+                                                                          motion_flag, 
+                                                                          object_flag)
+            handler = self.handlers[newState]
 
-                else:
-                    PiRun.context["count"] = 0
-                    np_image = (
-                        picam.capture_image()
-                    )  # return a fresh capture in ndarray image
-                    PiRun.ch_to_object_detecting()
-                    pred, file_path = image_pred(
-                        yolov5s_224_edge, np_image
-                    )  # output of detected bbox and file name
-                    if len(pred) == 0:  # nothing detected
-                        pass
-                        # time.sleep(10)  # cooling time if needed
-                    else:  # object detected
-                        PiRun.ch_to_object_detected()
-                        save_to_csv(
-                            file_path
-                        )  # save the summary of detection results in csv
-                        PiRun.ch_to_idle()
-                        time.sleep(1)  # cooling time if needed
+# Define state handlers and variables
 
+def state_motion_detecting(idle_count, motion_flag, object_flag):
+    print("Now is motion detecting stage.")
+    motion_flag = picam.motion_detection()  # check motion is detected or not
+    if motion_flag:
+        Image_Buffer = (
+            picam.capture_image()
+        )  # return a fresh capture in ndarray image
+        return(("object_detecting", idle_count, motion_flag, object_flag))
+    else:
+        return(("motion_not_detected", idle_count, motion_flag, object_flag))
 
-# Initization of PiCamera
-picam = picam2()
+def state_motion_not_detected(idle_count, motion_flag, object_flag):
+    print("Now is motion not detected stage.")
+    idle_count += 1
+    if (idle_count <= Idle_Max):
+        return(("state_idle", idle_count, motion_flag, object_flag))
+    else:
+        return(("object_detecting", idle_count, motion_flag, object_flag))
+
+def state_object_detecting(idle_count, motion_flag, object_flag):
+    print("Now is object detecting stage.")
+    global Image_Buffer  # explicitly declare Image_Buffer is global variable
+    global File_Path  # explicitly declare File_Path is global variable
+    print(idle_count)
+    Image_Buffer = picam.capture_image() if (idle_count - 1) == Idle_Max else Image_Buffer
+    (Pred, File_Path) = image_pred(
+        yolov5s_224_edge, Image_Buffer
+    )  # output of detected bbox and file name
+    
+    if (idle_count -1 ) == Idle_Max:
+        idle_count = 0  # reset counter only if it reach max
+
+    object_flag = True if len(Pred) != 0 else False
+    motion_flag = False  # reset flag
+
+    if object_flag:
+        return(("object_detected", idle_count, motion_flag, object_flag))
+    else:
+        return(("motion_detecting", idle_count, motion_flag, object_flag))
+
+def state_object_detected(idle_count, motion_flag, object_flag):
+    print("Now is object detected stage.")
+    global File_Path
+    save_to_csv(File_Path)  # save the summary of detection results in csv
+    return(("state_idle", idle_count, motion_flag, object_flag))
+
+def state_idle(idle_count, motion_flag, object_flag):
+    print("Now is idle stage.")
+    time.sleep(TIME_DELAY)  # cooling time after detected object if needed
+    if object_flag:
+        object_flag = False  # reset flag
+        return(("motion_detecting", idle_count, motion_flag, object_flag))
+    elif idle_count == 0:
+        print("Initiatizing...")
+        return(("motion_detecting", idle_count, motion_flag, object_flag))  
+    else:
+        print("Idle stage {} of {}.".format(idle_count, Idle_Max))
+        return(("motion_detecting", idle_count, motion_flag, object_flag))
+
+# Initiation of Object Detection State Machine
+def fsm_init(obj):
+
+    obj.add_state("motion_detecting", state_motion_detecting )
+    obj.add_state("motion_not_detected", state_motion_not_detected )
+    obj.add_state("object_detecting", state_object_detecting )
+    obj.add_state("object_detected", state_object_detected )
+    obj.add_state("state_idle", state_idle )
 
 # Initization of edge TPU and loading model
 """
@@ -181,11 +127,11 @@ yolov5s_224_edge = EdgeTPUModel(
     max_det=1000,
 )  # maximum detection objects
 
-# File path for saving the annotation file
-file_path = "./data/images"
-
 if __name__ == "__main__":  # if run the below if directly exceute this py
-    # Kick of the machine from motion detect state
-    PiRun = PiObjectDetection()
-    # Start run the object detection
-    PiRun.run()
+    # Initization of PiCamera
+    picam = picam2()
+    # Initization of object detection state machine
+    PiObjectDetection = fsm()
+    fsm_init(PiObjectDetection)
+    # Start runing the object detection
+    PiObjectDetection.run("state_idle")
